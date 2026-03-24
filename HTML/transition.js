2d53176eaea36f244551b63704e92511310b867c
Modou fall <serignemodoufall1@gmail.com>
2026-03-24 14:08:24 +0100

    u

--- HTML/transition.js
+++ HTML/transition.js
@@ -1,6 +1,15 @@
 const menu = document.getElementById("menu");
 const menuList = document.getElementById("menu-list");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", (e) => {
+  e.stopPropagation();
   menuList.classList.toggle("active");
 });
+
+document.body.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+// menu.addEventListener("click", => (e){
+//     e.stopPropagation()
+// })
