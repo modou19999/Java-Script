99d46d0d0f3eae07ec8b98c26394520c97c617ee
modou19999 <serignemodoufall1@gmail.com>
2026-06-01 15:54:42 +0200

    Update

--- 1.HTML/javascrip.js
+++
@@ -1,7 +0,0 @@
-
-      const toggle = document.getElementById("menu-toggle");
-      const navLinks = document.getElementById("nav-links");
-
-      toggle.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-      });
